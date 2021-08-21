2d2a380c3534be054a3ca72a4035134a85eebba1
yuesha-yc <kevinwangyichen@gmail.com>
2021-08-21 17:14:17 +0800

    Update clientscripts

--- kubejs/client_scripts/jei_hide_items.js
+++ kubejs/client_scripts/jei_hide_items.js
@@ -19,12 +19,12 @@ onEvent('jei.hide.items', (event) => {
     });
 
     serverItemsToHide.forEach((disabledItem) => {
-        if (disabledItem.toString.startsWith("#")) {
-            var tag = Ingredient.of(disabledItem);
-            tag.stacks.forEach((item) => {
-                event.hide(item);
-            })
-        }
+        // if (disabledItem.toString.startsWith("#")) {
+        //     var tag = Ingredient.of(disabledItem);
+        //     tag.stacks.forEach((item) => {
+        //         event.hide(item);
+        //     })
+        // }
         
         if (!Item.of(disabledItem).isEmpty()) {
             event.hide(disabledItem);
