eb315ca6f65dbc42005ad9f32debbdcde7eadd6f
yuesha-yc <kevinwangyichen@gmail.com>
2021-08-27 00:15:41 +0800

    Steam Age 1

--- kubejs/server_scripts/the_winter_rescue/recipetypes/custommachinery/steam_production.js
+++ kubejs/server_scripts/the_winter_rescue/recipetypes/custommachinery/steam_production.js
@@ -3,19 +3,19 @@ onEvent('recipes', event => {
     event.recipes.custommachinery.custom_machine("the_winter_rescue:bronze_boiler", 200)
     .requireBlock("custommachinery:custom_machine_block{machineID:\"the_winter_rescue:bronze_burner\",craftingManager:{status:\"running\"}}", 0, -1, 0, 0, 0, 0, 1, "==")
     .requireFluidPerTick(Fluid.of("minecraft:water", 1), "watertank")
-    .produceFluidPerTick(Fluid.of("kubejs:steam", 1), "steamtank")
+    .produceFluidPerTick(Fluid.of("steampowered:steam", 40), "steamtank")
     .jei();
 
     event.recipes.custommachinery.custom_machine("the_winter_rescue:cast_iron_boiler", 200)
         .requireBlock("custommachinery:custom_machine_block{machineID:\"the_winter_rescue:cast_iron_burner\",craftingManager:{status:\"running\"}}", 0, -1, 0, 0, 0, 0, 1, "==")
         .requireFluidPerTick(Fluid.of("minecraft:water", 2), "watertank")
-        .produceFluidPerTick(Fluid.of("kubejs:steam", 2), "steamtank")
+        .produceFluidPerTick(Fluid.of("steampowered:steam", 60), "steamtank")
         .jei();
 
     event.recipes.custommachinery.custom_machine("the_winter_rescue:steel_boiler", 200)
         .requireBlock("custommachinery:custom_machine_block{machineID:\"the_winter_rescue:steel_burner\",craftingManager:{status:\"running\"}}", 0, -1, 0, 0, 0, 0, 1, "==")
         .requireFluidPerTick(Fluid.of("minecraft:water", 4), "watertank")
-        .produceFluidPerTick(Fluid.of("kubejs:steam", 4), "steamtank")
+        .produceFluidPerTick(Fluid.of("steampowered:steam", 60), "steamtank")
         .jei();
 
     event.recipes.custommachinery.custom_machine("the_winter_rescue:bronze_burner", 200)
