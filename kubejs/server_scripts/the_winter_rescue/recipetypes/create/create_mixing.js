2b11ad89fb9cd88b55bb1a103bdca2f850f6e9c2
yuesha-yc <kevinwangyichen@gmail.com>
2021-09-11 21:01:48 +0800

    0.3.0 #Hotfix5

--- kubejs/server_scripts/the_winter_rescue/recipetypes/create/create_mixing.js
+++ kubejs/server_scripts/the_winter_rescue/recipetypes/create/create_mixing.js
@@ -1,7 +1,7 @@
 onEvent('recipes', (event) => {
     event.recipes.createMixing(
         ['rankine:high_refractory_brick'],
-        ['4x rankine:fire_clay_ball', '2x rankine:magnesia', '2x rankine:quicklime']).heated()
+        ['4x rankine:fire_clay_ball', '2x rankine:magnesia', '2x rankine:quicklime'])
 
     event.recipes.createMixing(
         ['3x rankine:fire_clay_ball'],
