04e1c5958809811d2f173dc78e24f45766a25c42
dashuaibia <936336596@qq.com>
2021-10-02 20:18:46 +0800

    js2.1

--- kubejs/client_scripts/ponder/ponder_tag_registry.js
+++ kubejs/client_scripts/ponder/ponder_tag_registry.js
@@ -3,4 +3,5 @@ onEvent("ponder.tag.registry", event => {
     event.create("the_winter_rescue:generator", "frostedheart:generator_core_t1", "", "")
     event.create("the_winter_rescue:crucible", "frostedheart:burning_chamber_core", "", "")
 	event.create("the_winter_rescue:steam_turbine", "immersiveengineering:generator", "", "")
+	event.create("the_winter_rescue:generator_t2", "rankine:invar_block", "", "")
 })
