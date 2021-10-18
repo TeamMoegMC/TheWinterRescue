a51b7757671b4f04f74aff3698810b18197101f3
duck_egg <changxietian@163.com>
2021-10-18 17:28:34 +0800

    把一段代码扔到循环里了

--- kubejs/server_scripts/the_winter_rescue/loot_tables/block_loot_tables.js
+++ kubejs/server_scripts/the_winter_rescue/loot_tables/block_loot_tables.js
@@ -1,5 +1,5 @@
-var ore_blocks = ["rankine:magnetite_ore", "rankine:pyrite_ore", "rankine:native_copper_ore", "rankine:malachite_ore", "rankine:pentlandite_ore", "rankine:native_tin_ore", "rankine:cassiterite_ore", "rankine:bauxite_ore", "rankine:stibnite_ore", "rankine:cinnabar_ore", "rankine:magnesite_ore", "rankine:galena_ore", "rankine:halite_ore", "rankine:fluorite_ore", "rankine:vanadinite_ore", "rankine:native_silver_ore", "rankine:native_gold_ore"];
-var ores = ["rankine:magnetite", "kubejs:pyrite", "kubejs:native_copper", "rankine:malachite", "rankine:pentlandite", "kubejs:native_tin", "rankine:cassiterite", "kubejs:bauxite", "rankine:stibnite", "rankine:cinnabar", "rankine:magnesite", "rankine:galena", "kubejs:halite", "rankine:fluorite", "rankine:vanadinite", "kubejs:silver", "kubejs:gold"];
+var ore_blocks = ["rankine:magnetite_ore", "rankine:pyrite_ore", "rankine:native_copper_ore", "rankine:malachite_ore", "rankine:pentlandite_ore", "rankine:native_tin_ore", "rankine:cassiterite_ore", "rankine:bauxite_ore", "rankine:stibnite_ore", "rankine:cinnabar_ore", "rankine:magnesite_ore", "rankine:galena_ore", "rankine:halite_ore", "rankine:fluorite_ore", "rankine:vanadinite_ore", "rankine:native_silver_ore", "rankine:native_gold_ore", "rankine:sphalerite_ore"];
+var ores = ["rankine:magnetite", "kubejs:pyrite", "kubejs:native_copper", "rankine:malachite", "rankine:pentlandite", "kubejs:native_tin", "rankine:cassiterite", "kubejs:bauxite", "rankine:stibnite", "rankine:cinnabar", "rankine:magnesite", "rankine:galena", "kubejs:halite", "rankine:fluorite", "rankine:vanadinite", "kubejs:silver", "kubejs:gold", "rankine:sphalerite"];
 var i;
 var ores_i;
 var ore_blocks_i;
@@ -16,14 +16,8 @@ onEvent('block.loot_tables', event => {
             })
         })
     }
+//若要修改矿石掉落物，将矿石本身放在ore_blicks里，掉落物放在ores里。貌似必须是双引号才行(
 
-    event.addBlock('rankine:sphalerite_ore', table => {
-        table.addPool(pool => {
-            pool.rolls = 1
-            pool.survivesExplosion()
-            pool.addItem('rankine:sphalerite')
-        })
-    })
 	
 	event.modifyBlock('primalwinter:snowy_dirt', table => {
 		table.addPool(pool=> {
