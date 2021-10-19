d396ce445a51b0dbbc2716daf9c97cf41cd25130
yuesha-yc <kevinwangyichen@gmail.com>
2021-10-19 04:34:09 -0700

    ftbq 4

--- ftbq_processor.py
+++ ftbq_processor.py
@@ -1,11 +1,11 @@
 import os
 
 ftbquests_path = "config/ftbquests/quests/chapters"
-zh_cn_path = "kubejs/assets/twr_quests/lang/zh_cn.lang"
-en_us_path = "kubejs/assets/twr_quests/lang/en_us.lang"
+zh_cn_path = "kubejs/assets/twr_quests/lang/zh_cn.json"
+en_us_path = "kubejs/assets/twr_quests/lang/en_us.json"
 should_replace_key = [
-    "title",
-    "subtitle"
+    "			title",
+    "			subtitle"
 ]
 context_dict = {}
 
@@ -25,48 +25,63 @@ def read_snbt(full_path, file_name):
     file_name = file_name.split(".snbt")[0]
     f.close()
 
-    quest_flag = False
-    for n, line in enumerate(f_list):
-        if line.startswith("		}"):
-            quest_flag = False
+    quests_list = []
+    quests_start_index_list = []
+    quest_start_line = 0
+    quest_end_line = 0
+    found_quest_start = False
+    found_quest_end = False
+    for i, line in enumerate(f_list):
 
-        flag = False  # is true, then read quest text
-        j = 0
-        if (quest_flag):
-            if (line.lstrip().startswith("]")):
-                flag = False
-                j = 0
-            if (flag):
-                j += 1
-                text_key = "description." + str(j)
-                replace_with_lang_key(line, text_key, f_list, i, file_name)
-                continue
-            for key in should_replace_key:
-                if (line.lstrip().startswith(key)):
-                    replace_with_lang_key(line, key, f_list, i, file_name)
-            if (line.lstrip().startswith("description:")):
-                flag = True
+        if found_quest_start and found_quest_end:
+            quests_list.append(f_list[quest_start_line:quest_end_line+1])
+            quests_start_index_list.append(quest_start_line)
 
         if line.startswith("		{"):
-            quest_flag = True
+            quest_start_line = i
+            found_quest_start = True
 
+        if line.startswith("		}"):
+            quest_end_line = i
+            found_quest_end = True
 
-    # flag = False  # is true, then read quest text
-    # j = 0
-    # for i, line in enumerate(f_list):
-    #     if (line.lstrip().startswith("]")):
-    #         flag = False
-    #         j = 0
-    #     if (flag):
-    #         j += 1
-    #         text_key = "description." + str(j)
-    #         replace_with_lang_key(line, text_key, f_list, i, file_name)
-    #         continue
-    #     for key in should_replace_key:
-    #         if (line.lstrip().startswith(key)):
-    #             replace_with_lang_key(line, key, f_list, i, file_name)
-    #     if (line.lstrip().startswith("description:")):
-    #         flag = True
+    for k, quest in enumerate(quests_list):
+        q_start_line_index = quests_start_index_list[k]
+        quest_id = ""
+
+        for m, find_id in enumerate(quest):
+            if find_id.startswith("			id"):
+                first_quote_index = find_id.find("\"")
+                last_quote_index = find_id.rfind("\"")
+                quest_id = find_id[first_quote_index + 1:last_quote_index]
+
+        for n, quest_line in enumerate(quest):
+            flag = False  # is true, then read quest text
+            is_multiple_line = False
+            j = 0
+            if quest_line.startswith("			description"):
+                flag = True
+                if not quest_line.endswith("]"):
+                    is_multiple_line = True
+            if is_multiple_line and quest_line.startswith("			]"):
+                flag = False
+                is_multiple_line = False
+                j = 0
+            if flag:
+                j += 1
+                if is_multiple_line:
+                    text_key = "description." + str(j)
+                    replace_with_lang_key(quest_line, text_key, f_list, q_start_line_index + n, quest_id)
+                    continue
+                else:
+                    flag = False
+                    text_key = "description." + str(j)
+                    replace_with_lang_key(quest_line, text_key, f_list, q_start_line_index + n, quest_id)
+                    continue
+
+            for key in should_replace_key:
+                if quest_line.startswith(key):
+                    replace_with_lang_key(quest_line, key.lstrip(), f_list, q_start_line_index + n, quest_id)
 
     f = open(full_path, "w+", encoding="utf-8")
     f.writelines(f_list)
