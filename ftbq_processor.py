560d72a1977be69384103ef758c67c8e3b72d375
yuesha-yc <kevinwangyichen@gmail.com>
2021-10-21 19:58:20 -0700

    ftbq 8, works properly now

--- ftbq_processor.py
+++ ftbq_processor.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 ftbquests_path = "config/ftbquests/quests/chapters"
 zh_cn_path = "kubejs/assets/twr_quests/lang/zh_cn.json"
@@ -7,7 +8,9 @@ should_replace_key = [
     "			title",
     "			subtitle"
 ]
+
 context_dict = {}
+total_lines = 0
 
 
 def check_dir(path):
@@ -25,6 +28,22 @@ def read_snbt(full_path, file_name):
     file_name = file_name.split(".snbt")[0]
     f.close()
 
+    # replace chapter translation keys
+    for i, line in enumerate(f_list):
+        chapter_id = ""
+        if line.startswith("\tid"):
+            first_quote_index = line.find("\"")
+            last_quote_index = line.rfind("\"")
+            chapter_id = line[first_quote_index + 1:last_quote_index]
+            print("=======================================")
+            print("Found chapter with ID \"%s\"" % (chapter_id))
+        if line.startswith("\ttitle"):
+            replace_with_lang_key(line, "title", f_list, i, chapter_id)
+        if line.startswith("\tsubtitle"):
+            replace_with_lang_key(line, "subtitle", f_list, i, chapter_id)
+
+
+    # enumerate file and collect quest list
     quests_list = []
     quests_start_index_list = []
     quest_start_line = 0
@@ -34,11 +53,8 @@ def read_snbt(full_path, file_name):
     for i, line in enumerate(f_list):
 
         if found_quest_start and found_quest_end:
-            print(f_list[quest_start_line:quest_end_line+1])
-            quests_list.append(f_list[quest_start_line:quest_end_line+1])
+            quests_list.append(f_list[quest_start_line:quest_end_line + 1])
             quests_start_index_list.append(quest_start_line)
-            # found_quest_start = False
-            # found_quest_end = False
 
         if line.startswith("		{"):
             quest_start_line = i
@@ -48,6 +64,7 @@ def read_snbt(full_path, file_name):
             quest_end_line = i
             found_quest_end = True
 
+    # enumerate quest list and replace translation keys
     for k, quest in enumerate(quests_list):
         q_start_line_index = quests_start_index_list[k]
         quest_id = ""
@@ -57,32 +74,40 @@ def read_snbt(full_path, file_name):
                 first_quote_index = find_id.find("\"")
                 last_quote_index = find_id.rfind("\"")
                 quest_id = find_id[first_quote_index + 1:last_quote_index]
+                print("------------------------------------")
+                print("Found quest with ID %s" % (quest_id))
 
         flag = False  # is true when this is a multiple line description
         j = 0
         for n, quest_line in enumerate(quest):
 
+            # checks whether this line is a single-line description
             if quest_line.startswith("			description: [\""):
                 text_key = "description.1"
                 replace_with_lang_key(quest_line, text_key, f_list, q_start_line_index + n, quest_id)
 
+            # checks whether this line ends a multiple-line description
             if quest_line.startswith("			]"):
                 flag = False
                 j = 0
 
+            # checks whether last line is the start of a multiple-line description
+            # or part of the multiple-line description
+            # if it is, starts replacing
             if flag:
                 j += 1
                 text_key = "description." + str(j)
                 replace_with_lang_key(quest_line, text_key, f_list, q_start_line_index + n, quest_id)
                 continue
 
+            # checks whether this line is a title or subtitle
             for key in should_replace_key:
                 if quest_line.startswith(key):
                     replace_with_lang_key(quest_line, key.lstrip(), f_list, q_start_line_index + n, quest_id)
 
+            # checks whether this line is the start of a multiple-line description
             if quest_line == "\t\t\tdescription: [\n":
                 flag = True
-                # continue
 
     f = open(full_path, "w+", encoding="utf-8")
     f.writelines(f_list)
@@ -90,49 +115,40 @@ def read_snbt(full_path, file_name):
 
 
 def replace_with_lang_key(line, key, f_list, index, file_name):
-    print("LINE: "+line)
+    global total_lines
+    total_lines += 1
     first_quote_index = line.find("\"")
-    print("FQ:   " + str(first_quote_index))
     last_quote_index = line.rfind("\"")
-    print("LQ:   " + str(last_quote_index))
     head = line[0:first_quote_index]
     content = line[first_quote_index + 1:last_quote_index]
-    print("CONTENT: "+content)
     tail = line[last_quote_index + 1:len(line)]
     if not (content.startswith("{") and content.endswith("}")):
         lang_key = "quest.twr.%s.%s" % (file_name, key)
-        print("get lang key %s, value = %s" % (lang_key, content))
+        print("Found translation key \"%s\", value = \"%s\"" % (lang_key, content))
         new_content = head + "\"{" + lang_key + "}\"" + tail
         f_list[index] = new_content
         context_dict[lang_key] = content.replace("\\", "")
 
 
-def write_lang(path):
+def write_json(path):
     copy = context_dict.copy()
-    if (len(copy) == 0):
+    if len(copy) == 0:
         return
-    to_append_entries = []
-    f = open(path, "r+", encoding='utf-8')
-    f_list = f.readlines()
-    f.close()
-    f_list_copy = f_list.copy()
-    for i, line in enumerate(f_list):
-        if (line.startswith("#")):
-            continue
-        key = line.split("=")[0]
-        if (key in copy):
-            f_list_copy[i] = key + "=" + copy.pop(key) + "\n"
-    for key, value in copy.items():
-        to_append_entries.append(key + "=" + value)
-    f = open(path, "w+", encoding="utf-8")
-    f.writelines(f_list_copy)
-    if (len(f_list[-1]) != 0 and f_list[-1] != "\n"):
-        f.write("\n")
-    for entry in to_append_entries:
-        f.write(entry + "\n")
-    f.close()
+
+    # some preinit stuff
+    copy["group.twr.main_line.title"] = "Main Line"
+    copy["group.twr.branch_line.title"] = "Branch Line"
+    if path.endswith("zh_cn.json"):
+        copy["group.twr.main_line.title"] = "主线"
+        copy["group.twr.branch_line.title"] = "支线"
+
+    with open(path, "w", encoding="utf-8") as outfile:
+        json.dump(copy, outfile, indent=2, ensure_ascii=False)
+
+    global total_lines
+    print("Successfully translated " + str(total_lines) + " lines in " + path)
 
 
 check_dir(ftbquests_path)
-write_lang(zh_cn_path)
-write_lang(en_us_path)
+write_json(zh_cn_path)
+write_json(en_us_path)
