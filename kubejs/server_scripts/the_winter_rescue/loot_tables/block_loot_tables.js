e38439dcfe1ad50301b4a768d03d3ad4c44509cf
yuesha-yc <kevinwangyichen@gmail.com>
2021-11-03 00:03:20 -0700

    fix sphalerite loot

--- kubejs/server_scripts/the_winter_rescue/loot_tables/block_loot_tables.js
+++ kubejs/server_scripts/the_winter_rescue/loot_tables/block_loot_tables.js
@@ -1,20 +1,60 @@
-var ore_blocks = ["rankine:magnetite_ore", "rankine:pyrite_ore", "rankine:native_copper_ore", "rankine:malachite_ore", "rankine:pentlandite_ore", "rankine:native_tin_ore", "rankine:cassiterite_ore", "rankine:bauxite_ore", "rankine:stibnite_ore", "rankine:cinnabar_ore", "rankine:magnesite_ore", "rankine:galena_ore", "rankine:halite_ore", "rankine:fluorite_ore", "rankine:vanadinite_ore", "rankine:native_silver_ore", "rankine:native_gold_ore", "rankine:sphalerite_ore"];
-var ores = ["rankine:magnetite", "kubejs:pyrite", "kubejs:native_copper", "rankine:malachite", "rankine:pentlandite", "kubejs:native_tin", "rankine:cassiterite", "kubejs:bauxite", "rankine:stibnite", "rankine:cinnabar", "rankine:magnesite", "rankine:galena", "kubejs:halite", "rankine:fluorite", "rankine:vanadinite", "kubejs:silver", "kubejs:gold", "rankine:sphalerite"];
+var ore_blocks = [
+"rankine:magnetite_ore", 
+"rankine:pyrite_ore", 
+"rankine:native_copper_ore", 
+"rankine:malachite_ore", 
+"rankine:pentlandite_ore", 
+"rankine:native_tin_ore", 
+"rankine:cassiterite_ore", 
+"rankine:bauxite_ore", 
+"rankine:stibnite_ore", 
+"rankine:cinnabar_ore", 
+"rankine:magnesite_ore", 
+"rankine:galena_ore", 
+"rankine:halite_ore", 
+"rankine:fluorite_ore", 
+"rankine:vanadinite_ore", 
+"rankine:native_silver_ore", 
+"rankine:native_gold_ore", 
+// "rankine:sphalerite_ore"
+];
+var ores = [
+"rankine:magnetite", 
+"kubejs:pyrite", 
+"kubejs:native_copper", 
+"rankine:malachite", 
+"rankine:pentlandite", 
+"kubejs:native_tin", 
+"rankine:cassiterite", 
+"kubejs:bauxite", 
+"rankine:stibnite", 
+"rankine:cinnabar", 
+"rankine:magnesite", 
+"rankine:galena", 
+"kubejs:halite", 
+"rankine:fluorite", 
+"rankine:vanadinite", 
+"kubejs:silver", 
+"kubejs:gold", 
+// "rankine:sphalerite"
+];
 var i;
 var ores_i;
 var ore_blocks_i;
 
 onEvent('block.loot_tables', event => {
-    for (i = 0; i < ores.length; i++) {
+    for (i = 0; i <= ores.length; i++) {
         ores_i = ores[i];
         ore_blocks_i = ore_blocks[i];
-        event.addBlock(ore_blocks_i, table => {
-            table.addPool(pool => {
-                pool.rolls = 1
-                pool.survivesExplosion()
-                pool.addItem(ores_i)
-            })
-        })
+
+        event.addSimpleBlock(ore_blocks_i, ores_i)
+        // event.addBlock(ore_blocks_i, table => {
+        //     table.addPool(pool => {
+        //         pool.rolls = 1
+        //         pool.survivesExplosion()
+        //         pool.addItem(ores_i)
+        //     })
+        // })
     }
 //若要修改矿石掉落物，将矿石本身放在ore_blicks里，掉落物放在ores里。貌似必须是双引号才行(
 
