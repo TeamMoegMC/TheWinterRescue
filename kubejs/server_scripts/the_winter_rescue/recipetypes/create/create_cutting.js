e383464c14807e8ad62223320b1164c11352d3d1
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2021-11-13 12:12:29 +0800

    Plank automato!

--- kubejs/server_scripts/the_winter_rescue/recipetypes/create/create_cutting.js
+++ kubejs/server_scripts/the_winter_rescue/recipetypes/create/create_cutting.js
@@ -1,18 +1,7 @@
 onEvent('recipes', (event) => {
     var data = {
         recipes: [
-        {output:'4x rankine:cinnamon_planks', input: 'rankine:cinnamon_log'},
-        {output:'4x rankine:cedar_planks', input: 'rankine:cedar_log'},
-        {output:'4x rankine:sharinga_planks', input: 'rankine:sharinga_log'},
-        {output:'4x rankine:black_walnut_planks', input: 'rankine:black_walnut_log'},
-        {output:'4x rankine:cork_oak_planks', input: 'rankine:cork_oak_log'},
-        {output:'4x rankine:coconut_palm_planks', input: 'rankine:coconut_palm_log'},
-        {output:'4x rankine:pinyon_pine_planks', input: 'rankine:pinyon_pine_log'},
-        {output:'4x rankine:juniper_planks', input: 'rankine:juniper_log'},
-        {output:'4x rankine:balsam_fir_planks', input: 'rankine:balsam_fir_log'},
-        {output:'4x rankine:magnolia_planks', input: 'rankine:magnolia_log'},
-        {output:'4x rankine:eastern_hemlock_planks', input: 'rankine:eastern_hemlock_log'},
-        {output:'4x rankine:maple_planks', input: 'rankine:maple_log'},
+
 		{output:'9x minecraft:iron_ingot',input:'minecraft:iron_block'},
 		{output:'9x minecraft:gold_ingot',input:'minecraft:gold_block'},
 		{output:'9x minecraft:diamond',input:'minecraft:diamond_block'},
@@ -21,16 +10,21 @@ onEvent('recipes', (event) => {
 		{output:'9x minecraft:netherite_ingot',input:'minecraft:netherite_ingot'}
         ]
     };
-	let woods=[
-		"minecraft:jungle",
-		"minecraft:oak",
-		"minecraft:spruce",
-		"minecraft:birch",
-		"minecraft:jungle",
-		"minecraft:acacia",
-		"minecraft:dark_oak"
-	]
+    const woods = []
+	//auto bulk slab recipes
+	Ingredient.of("#minecraft:planks").getItemIds().forEach((name)=>{
+		woods.push(name.replace("_planks",""));
+	});
+	Ingredient.of("#minecraft:logs").getItemIds().forEach((name)=>{
+		if(name.indexOf("log")!=-1)
+		event.recipes.createCutting("4x "+name.replace("_log","").replace("stripped_","")+"_planks",name);
+		else if(name.indexOf("wood")!=-1)
+		event.recipes.createCutting("4x "+name.replace("_wood","").replace("stripped_","")+"_planks",name);
+		else if(name.indexOf("stem")!=-1)
+		event.recipes.createCutting("4x "+name.replace("_stem","").replace("stripped_","")+"_planks",name);
+	});
 	woods.forEach((wood)=>{
+		event.remove({output: wood+"_slab", type: 'minecraft:crafting_shaped'});
 		data.recipes.push({output:'2x '+wood+"_slab",input:wood+"_planks"})
 	});
     data.recipes.forEach((recipe) => {
--- kubejs/server_scripts/the_winter_rescue/recipetypes/stoneage/tree_stump.js
+++ kubejs/server_scripts/the_winter_rescue/recipetypes/stoneage/tree_stump.js
@@ -29,50 +29,27 @@ onEvent('recipes', (event) => {
 		tree_stumpRecipe(Item.of("3x rankine:sawdust"), Ingredient.of("#minecraft:logs"),
             Ingredient.of('#forge:hammers'), 2),
         tree_stumpRecipe(Item.of('immersiveengineering:slab_treated_wood_horizontal', 2), Item.of('immersiveengineering:treated_wood_horizontal'),
-            Ingredient.of('#forge:axes'), 2),
-        tree_stumpRecipe(Item.of('minecraft:dark_oak_planks', 3), Item.of('minecraft:stripped_dark_oak_log'),
-            Ingredient.of('#forge:axes'), 2),
-        tree_stumpRecipe(Item.of('minecraft:oak_planks', 3), Item.of('minecraft:stripped_oak_log'),
-            Ingredient.of('#forge:axes'), 2),
-        tree_stumpRecipe(Item.of('minecraft:acacia_planks', 3), Item.of('minecraft:stripped_dark_oak_log'),
-            Ingredient.of('#forge:axes'), 2),
-        tree_stumpRecipe(Item.of('minecraft:dark_oak_planks', 3), Item.of('minecraft:stripped_acacia_log'),
-            Ingredient.of('#forge:axes'), 2),
-        tree_stumpRecipe(Item.of('minecraft:birch_planks', 3), Item.of('minecraft:stripped_birch_log'),
-            Ingredient.of('#forge:axes'), 2),
-        tree_stumpRecipe(Item.of('minecraft:jungle_planks', 3), Item.of('minecraft:stripped_jungle_log'),
-            Ingredient.of('#forge:axes'), 2),
-        tree_stumpRecipe(Item.of('minecraft:spruce_planks', 3), Item.of('minecraft:stripped_spruce_log'),
             Ingredient.of('#forge:axes'), 2)
     ]
-    const wood_recipes = [
-        tree_stumpRecipe(Item.of('rankine:cinnamon_planks', 3), Item.of('rankine:stripped_cinnamon_log'),
-            Ingredient.of('#forge:axes'), 2),
-        tree_stumpRecipe(Item.of('rankine:cedar_planks', 3), Item.of('rankine:stripped_cedar_log'),
-            Ingredient.of('#forge:axes'), 2),
-        tree_stumpRecipe(Item.of('rankine:sharinga_planks', 3), Item.of('rankine:stripped_sharinga_log'),
-            Ingredient.of('#forge:axes'), 2),
-        tree_stumpRecipe(Item.of('rankine:black_walnut_planks', 3), Item.of('rankine:stripped_black_walnut_log'),
-            Ingredient.of('#forge:axes'), 2),
-        tree_stumpRecipe(Item.of('rankine:cork_oak_planks', 3), Item.of('rankine:stripped_cork_oak_log'),
-            Ingredient.of('#forge:axes'), 2),
-        tree_stumpRecipe(Item.of('rankine:coconut_palm_planks', 3), Item.of('rankine:stripped_coconut_palm_log'),
-            Ingredient.of('#forge:axes'), 2),
-        tree_stumpRecipe(Item.of('rankine:pinyon_pine_planks', 3), Item.of('rankine:stripped_pinyon_pine_log'),
-            Ingredient.of('#forge:axes'), 2),
-        tree_stumpRecipe(Item.of('rankine:juniper_planks', 3), Item.of('rankine:stripped_juniper_log'),
-            Ingredient.of('#forge:axes'), 2),
-        tree_stumpRecipe(Item.of('rankine:balsam_fir_planks', 3), Item.of('rankine:stripped_balsam_fir_log'),
-            Ingredient.of('#forge:axes'), 2),
-        tree_stumpRecipe(Item.of('rankine:magnolia_planks', 3), Item.of('rankine:stripped_magnolia_log'),
-            Ingredient.of('#forge:axes'), 2),
-        tree_stumpRecipe(Item.of('rankine:eastern_hemlock_planks', 3), Item.of('rankine:stripped_eastern_hemlock_log'),
-            Ingredient.of('#forge:axes'), 2),
-        tree_stumpRecipe(Item.of('rankine:maple_planks', 3), Item.of('rankine:stripped_maple_log'),
-            Ingredient.of('#forge:axes'), 2),
-        tree_stumpRecipe(Item.of('watersource:coconut_tree_planks', 3), Item.of('watersource:stripped_coconut_tree_log'),
-            Ingredient.of('#forge:axes'), 2)
-    ]
+    const wood_recipes = []
+	//auto bulk wood recipes
+	Ingredient.of("#minecraft:logs").getItemIds().forEach((name)=>{
+		if(name.indexOf("log")!=-1){
+			let namex=name.replace("_log","").replace("stripped_","");
+			wood_recipes.push(tree_stumpRecipe(Item.of(namex+'_planks', 3), Item.of(name),
+				Ingredient.of('#forge:axes'), 2))
+		}else if(name.indexOf("wood")!=-1){
+			let namex=name.replace("_wood","").replace("stripped_","");
+			wood_recipes.push(tree_stumpRecipe(Item.of(namex+'_planks', 3), Item.of(name),
+				Ingredient.of('#forge:axes'), 2))
+		}else if(name.indexOf("stem")!=-1){
+			let namex=name.replace("_stem","").replace("stripped_","");
+			wood_recipes.push(tree_stumpRecipe(Item.of(namex+'_planks', 3), Item.of(name),
+				Ingredient.of('#forge:axes'), 2))
+		}
+		
+	});
+	
     wood_recipes.forEach((recipe) => {
         event.remove({output: recipe.result, type: 'minecraft:crafting_shapeless'});
         event.custom({
