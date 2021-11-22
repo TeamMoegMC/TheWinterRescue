a4b400c473dbdcbe41302041aee95640b96b6c9f
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2021-11-22 12:20:40 +0800

    changed priority

--- kubejs/server_scripts/the_winter_rescue/recipes/shapeless.js
+++ kubejs/server_scripts/the_winter_rescue/recipes/shapeless.js
@@ -65,9 +65,9 @@ onEvent('recipes', (event) => {
 	 event.recipes.createCompacting(["3x kubejs:creosoted_coal_briquette"], ["#forge:dusts/coal_coke",Fluid.of('immersiveengineering:creosote', 100)]);
 	 event.recipes.createCompacting(["kubejs:creosoted_charcoal_briquette"], ["kubejs:charcoal_dust",Fluid.of('immersiveengineering:creosote',250)]).heated();
 	 event.recipes.createCompacting(["kubejs:creosoted_charcoal_briquette"], ["kubejs:charcoal_dust",Fluid.of('immersiveengineering:creosote',260)]);
+         event.recipes.createCompacting(["kubejs:creosoted_sawdust_briquette"], ["4x #forge:dusts/wood",Fluid.of('immersiveengineering:creosote', 5]).heated()
 	 event.recipes.createCompacting(["kubejs:creosoted_sawdust_briquette"], ["4x #forge:sawdust",Fluid.of('immersiveengineering:creosote', 25)]);
 	 event.recipes.createCompacting(["kubejs:creosoted_sawdust_briquette"], ["4x #forge:dusts/wood",Fluid.of('immersiveengineering:creosote', 25)]);
-         event.recipes.createCompacting(["kubejs:creosoted_sawdust_briquette"], ["4x #forge:dusts/wood"]).heated();
 	 event.recipes.createCompacting(["4x kubejs:bound_coal_briquette"], ["2x #kubejs:fuel_glue","#forge:dusts/coal_coke"]);
 	 event.recipes.createCompacting(["5x kubejs:bound_coal_briquette"], ["2x #kubejs:fuel_glue","kubejs:anthracite_dust"]);
 	 event.recipes.createCompacting(["3x kubejs:bound_charcoal_briquette"], ["4x #kubejs:fuel_glue","2x kubejs:charcoal_dust"]);
