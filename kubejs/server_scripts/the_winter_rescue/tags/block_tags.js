46974e2e5343f1c48605084a83653588b4eca2cb
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2021-12-14 14:35:14 +0800

    wild

--- kubejs/server_scripts/the_winter_rescue/tags/block_tags.js
+++ kubejs/server_scripts/the_winter_rescue/tags/block_tags.js
@@ -23,8 +23,33 @@ let alwaysBreakableBlocks = [
     "minecraft:coarse_dirt",
     "#minecraft:sand"
 ]
-
+let snowyLeaveBlocks = [
+    "primalwinter:snowy_oak_leaves",
+    "primalwinter:snowy_birch_leaves",
+    "primalwinter:snowy_spruce_leaves",
+    "primalwinter:snowy_jungle_leaves",
+    "primalwinter:snowy_dark_oak_leaves",
+    "primalwinter:snowy_acacia_leaves"
+]
+let rankineLeaveBlocks=[
+	"rankine:cinnamon_leaves",
+    "rankine:sharinga_leaves",
+    "rankine:cork_oak_leaves",
+    "rankine:black_walnut_leaves",
+    "rankine:cedar_leaves",
+    "rankine:coconut_palm_leaves",
+    "rankine:pinyon_pine_leaves",
+    "rankine:juniper_leaves",
+    "rankine:balsam_fir_leaves",
+    "rankine:magnolia_leaves",
+    "rankine:eastern_hemlock_leaves",
+    "rankine:yellow_birch_leaves",
+    "rankine:black_birch_leaves",
+    "rankine:maple_leaves"
+]
 onEvent('block.tags', (event) => {
     event.add('primalwinter:animal_spawns_on', animalSpawnsOnBlocks)
+	event.add('kubejs:snowy_leaves', snowyLeaveBlocks)
+	event.add('kubejs:rankine_leaves',rankineLeaveBlocks)
 	event.add('forge:ores', 'rankine:pyrite_ore')
 });
