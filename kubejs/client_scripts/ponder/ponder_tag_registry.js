46c74f63c7b962bb1d2258962c56883a4bf8334a
Letter N <24603524+LetterN@users.noreply.github.com>
2022-01-04 09:43:46 +0800

    initial commit

--- kubejs/client_scripts/ponder/ponder_tag_registry.js
+++ kubejs/client_scripts/ponder/ponder_tag_registry.js
@@ -1,8 +1,11 @@
 onEvent("ponder.tag.registry", event => {
-    event.create("the_winter_rescue:bloomery", "charcoal_pit:sandy_brick", "", "")
-    event.create("the_winter_rescue:generator", "frostedheart:generator_core_t1", "", "")
+    event.create("the_winter_rescue:bloomery", "charcoal_pit:main_bloomery", "", "")
+
+    event.create("the_winter_rescue:generator", "frostedheart:generator", "", "")
+
     event.create("the_winter_rescue:crucible", "immersiveindustry:burning_chamber", "", "")
+
 	event.create("the_winter_rescue:steam_turbine", "immersiveengineering:generator", "", "")
-	event.create("the_winter_rescue:generator_t2", "rankine:invar_block", "", "")
+
 	event.create("the_winter_rescue:beehive_oven_pit", "rankine:beehive_oven_pit", "", "")
 })
