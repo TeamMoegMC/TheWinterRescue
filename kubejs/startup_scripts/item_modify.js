e65a94c3f93481f67aa492773c6707a9a0666e20
yuesha-yc <kevinwangyichen@gmail.com>
2022-09-01 17:56:48 -0700

    Add burn time to snowy vine (very low) Tweak quests

--- kubejs/startup_scripts/item_modify.js
+++ kubejs/startup_scripts/item_modify.js
@@ -1,23 +1,30 @@
-var carpets=["minecraft:white_carpet",
-"minecraft:orange_carpet",
-"minecraft:magenta_carpet",
-"minecraft:light_blue_carpet",
-"minecraft:yellow_carpet",
-"minecraft:lime_carpet",
-"minecraft:pink_carpet",
-"minecraft:gray_carpet",
-"minecraft:light_gray_carpet",
-"minecraft:cyan_carpet",
-"minecraft:purple_carpet",
-"minecraft:blue_carpet",
-"minecraft:brown_carpet",
-"minecraft:black_carpet",
-"minecraft:green_carpet",
-"minecraft:red_carpet"
+var carpets = ["minecraft:white_carpet",
+	"minecraft:orange_carpet",
+	"minecraft:magenta_carpet",
+	"minecraft:light_blue_carpet",
+	"minecraft:yellow_carpet",
+	"minecraft:lime_carpet",
+	"minecraft:pink_carpet",
+	"minecraft:gray_carpet",
+	"minecraft:light_gray_carpet",
+	"minecraft:cyan_carpet",
+	"minecraft:purple_carpet",
+	"minecraft:blue_carpet",
+	"minecraft:brown_carpet",
+	"minecraft:black_carpet",
+	"minecraft:green_carpet",
+	"minecraft:red_carpet"
 ];
 
 onEvent('item.modification', event => {
-  carpets.forEach(name=>{event.modify(name, item => {
-	item.burnTime = 0
-	})});
+	carpets.forEach(name => {
+		event.modify(name, item => {
+			item.burnTime = 0
+		})
+	});
+
+	// modify burn time of snowy vine, 20 ticks, which is half of straw
+	event.modify('primalwinter:snowy_vine', item => {
+		item.burnTime = 20
+	});
 })
