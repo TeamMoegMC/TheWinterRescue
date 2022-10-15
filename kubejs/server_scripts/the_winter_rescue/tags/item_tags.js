7ea748801d49c1812d5e1ff0b9a139792fa0148e
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2022-10-16 02:41:22 +0800

    tags

--- kubejs/server_scripts/the_winter_rescue/tags/item_tags.js
+++ kubejs/server_scripts/the_winter_rescue/tags/item_tags.js
@@ -123,9 +123,11 @@ onEvent('item.tags', (event) => {
 	event.add('thermopolium:container','frostedheart:advanced_thermos');
 	event.add('thermopolium:container','frostedheart:thermos');
 	event.add('thermopolium:cereals/baked','frostedheart:military_rations');
+	event.add('thermopolium:cereals/baked','frostedheart:rye_bread');
 	event.add('thermopolium:vegetables','frostedheart:military_rations');
 	event.add('thermopolium:meats/meat','frostedheart:military_rations');
 	event.add('thermopolium:meats/meat','charcoal_pit:chocolate');
+	event.add('thermopolium:vegetables','frostedheart:white_turnip_block');
 	event.add('thermopolium:vegetables','frostedheart:dried_vegetables');
 	event.add('frostedheart:mushroom_base',"frostedheart:straw_briquette_red_mushroom")
 	event.add('frostedheart:mushroom_base',"frostedheart:straw_briquette_brown_mushroom")
