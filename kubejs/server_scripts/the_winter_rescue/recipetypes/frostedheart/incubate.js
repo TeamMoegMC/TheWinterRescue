f041718131f16aec7958b8e3868d72c807a87f54
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2022-10-16 17:14:42 +0800

    rename recipe

--- kubejs/server_scripts/the_winter_rescue/recipetypes/frostedheart/incubate.js
+++ kubejs/server_scripts/the_winter_rescue/recipetypes/frostedheart/incubate.js
@@ -1,6 +1,6 @@
 onEvent('recipes', (event) => {
 //event.custom(inspireRecipe("kubejs:silver",1000));
 	Ingredient.of("#forge:edible_mushrooms").getItemIds().forEach((name)=>{
-	event.custom(incubateRecipeI(Ingredient.of("#kubejs:straw_briquettes"),Ingredient.of(name),false,Ingredient.of("8x "+name),2400,1))
+	event.custom(incubateRecipeI(Ingredient.of("#kubejs:straw_briquettes"),Ingredient.of(name),false,Ingredient.of("8x "+name),2400,1)).id("the_winter_rescue:incubate/"+name.split(":")[0]+"/"+name.split(":")[1]);
 	});
 });
