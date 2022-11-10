0c567aced60e691bf110788dc3ed94a4764352fc
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2022-11-10 22:29:54 +0800

    Sauna research and add loot

--- kubejs/server_scripts/the_winter_rescue/recipetypes/frostedheart/inspire.js
+++ kubejs/server_scripts/the_winter_rescue/recipetypes/frostedheart/inspire.js
@@ -1,13 +1,12 @@
 onEvent('recipes', (event) => {
-//event.custom(inspireRecipe("kubejs:silver",1000));
     var inspireRecipes = [
-        inspireRecipe("kubejs:damaged_gpu", 2250),
-        inspireRecipe("kubejs:damaged_circuit_board", 2000),
-        inspireRecipe("kubejs:damaged_electric_motor", 1500),
-        inspireRecipe("kubejs:damaged_camera", 1500),
-        inspireRecipe("kubejs:burnt_circuit_board", 2000),
-        inspireRecipe("kubejs:broken_robot_1", 2250),
-        inspireRecipe("kubejs:broken_robot_2", 2250)
+        inspireRecipe("kubejs:damaged_gpu", 1000),
+        inspireRecipe("kubejs:damaged_circuit_board", 600),
+        inspireRecipe("kubejs:damaged_electric_motor", 500),
+        inspireRecipe("kubejs:damaged_camera", 500),
+        inspireRecipe("kubejs:burnt_circuit_board", 600),
+        inspireRecipe("kubejs:broken_robot_1", 2000),
+        inspireRecipe("kubejs:broken_robot_2", 2000)
     ]
 
     inspireRecipes.forEach((recipe) => {
