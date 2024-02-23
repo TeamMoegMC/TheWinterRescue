95774ae6b5637800b43185573fe5d3cb0e0d3ced
Yichen Wang <kevinwangyichen@gmail.com>
2024-02-23 00:43:07 -0800

    Reduce alloy cost

--- kubejs/server_scripts/the_winter_rescue/recipetypes/immersiveengineering/alloy.js
+++ kubejs/server_scripts/the_winter_rescue/recipetypes/immersiveengineering/alloy.js
@@ -7,21 +7,21 @@ onEvent('recipes', (event) => {
                 time: 200
             },
             {input1: '#forge:stone', input2: '#forge:stone', output: Item.of('minecraft:stone', 2), time: 200},
-            {input1: 'minecraft:sand', input2: 'kubejs:charcoal_dust', output: 'minecraft:glass', time: 200},
+            {input1: '8x minecraft:sand', input2: 'kubejs:charcoal_dust', output: '8x minecraft:glass', time: 800},
             {
                 input1: '3x #forge:ingots/lead',
                 input2: '#forge:ingots/antimony',
                 output: '4x kubejs:battery_alloy',
                 time: 400
             },
-			{input1: '#forge:ingots/zinc', input2: '2x #forge:ingots/copper', output: '3x rankine:brass_ingot', time: 400},
+			{input1: '#forge:ingots/zinc', input2: '2x #forge:ingots/copper', output: '3x rankine:brass_ingot', time: 300},
             // {
             //     input1: '#forge:glass',
             //     input2: Ingredient.of('#forge:ingots/copper', 3),
             //     output: Item.of('tconstruct:tinkers_bronze_ingot', 3)
             // }
-            {input1: '#forge:ores/copper', input2: '2x #kubejs:kiln_dust', output: 'rankine:copper_ingot', time: 250},
-            {input1: '#forge:ores/tin', input2: '1x #kubejs:kiln_dust', output: 'rankine:tin_ingot', time: 250}
+            {input1: '#forge:ores/copper', input2: '#forge:ores/copper', output: '2x rankine:copper_ingot', time: 200},
+            {input1: '#forge:ores/tin', input2: '#forge:ores/tin', output: '2x rankine:tin_ingot', time: 200}
         ]
     recipes.forEach((recipe) => {
         event.recipes.immersiveengineering.alloy(recipe.output, recipe.input1, recipe.input2, recipe.time);
