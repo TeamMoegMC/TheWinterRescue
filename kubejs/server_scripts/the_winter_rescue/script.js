3ac72bfb76c6d64a58698163b40a0c420ba527d1
yuesha-yc <kevinwangyichen@gmail.com>
2024-10-27 22:29:26 -0700

    Update scripts

--- kubejs/server_scripts/the_winter_rescue/script.js
+++ kubejs/server_scripts/the_winter_rescue/script.js
@@ -5,26 +5,4 @@ settings.logRemovedRecipes = true
 settings.logSkippedRecipes = false
 settings.logErroringRecipes = true
 
-console.info('[KUBEJS_SERVER] Server Scripts Loaded/Reloaded!')
-/* Tutorial
-var x = event.variableName
-
-event.variableName = 10
-
-event.functionName()	
-
-event.functionName(10, 'abc')
-
-*/
-
-onEvent('recipes', event => {
-    // Change recipes here
-})
-
-onEvent('item.tags', event => {
-    // Get the #forge:cobblestone tag collection and add Diamond Ore to it
-    // event.get('forge:cobblestone').add('minecraft:diamond_ore')
-
-    // Get the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
-    // event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
-})
+console.info('[TWR_KUBEJS_SERVER] Server Scripts Loaded/Reloaded!')
