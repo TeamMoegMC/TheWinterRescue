819b4c76961225557c0bfc2fa0122183d6dceb61
yuesha-yc <kevinwangyichen@gmail.com>
2024-11-13 06:00:27 -0800

    2

--- kubejs/server_scripts/the_winter_rescue/recipes_packaged/frozen_things.js
+++ kubejs/server_scripts/the_winter_rescue/recipes_packaged/frozen_things.js
@@ -1,5 +1,5 @@
 //解冻
-onEvent('recipes', (event) => {
+ServerEvents.recipes(event => {
     var frozen_things = ["kubejs:frozen_auroch_meat", "kubejs:frozen_beef", "kubejs:frozen_boar_meat", "kubejs:frozen_chicken", "kubejs:frozen_fat", "kubejs:frozen_fowl_meat", "kubejs:frozen_mammoth_meat", "kubejs:frozen_mouflon_meat", "kubejs:frozen_tiger_meat", "kubejs:frozen_mutton", "kubejs:frozen_porkchop", "kubejs:frozen_rhino_meat", "kubejs:frozen_venison", "kubejs:frozen_rabbit", "kubejs:frozen_white_turnip"];
     var unfrozen_things = ["stone_age:auroch_meat", "minecraft:beef", "stone_age:boar_meat", "minecraft:chicken", "stone_age:fat", "stone_age:fowl_meat", "stone_age:mammoth_meat", "stone_age:mouflon_meat", "stone_age:tiger_meat", "minecraft:mutton", "minecraft:porkchop", "stone_age:rhino_meat", "stone_age:venison", "minecraft:rabbit", "frostedheart:white_turnip_block"];
     var i_unfreeze;
