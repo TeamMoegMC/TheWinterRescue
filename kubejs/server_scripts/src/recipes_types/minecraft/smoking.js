552b4ce6cae62b7b6255267cfddcf2a9d9882db1
yuesha-yc <kevinwangyichen@gmail.com>
2025-04-17 02:22:11 -0700

    Fix

--- kubejs/server_scripts/src/recipes_types/minecraft/smoking.js
+++ kubejs/server_scripts/src/recipes_types/minecraft/smoking.js
@@ -3,8 +3,6 @@ ServerEvents.recipes((event) => {
     minecraft.smoking(Item.of('frostedheart:wooden_cup_drink', '{Fluid:{FluidName:"caupona:nail_soup",Amount:250}}'), Item.of('frostedheart:wooden_cup_drink', '{Fluid:{FluidName:"minecraft:water",Amount:250}}'))
     minecraft.smoking('frostedheart:rye_bread', Item.of('frostedheart:raw_rye_bread'))
 
-    minecraft.smelting("minecraft:terracotta", "minecraft:clay")
-
     let recipes = [
         {output: 'frostedheart:alumina_dust', input: 'frostedheart:aluminium_hydroxide_dust'},
         {output: 'kubejs:coal_briquette', input: 'kubejs:wet_coal_briquette'},
