706dbe16f20f0bd04e3f445bf99ee2341ef4e6d7
dasb <936336596@qq.com>
2025-05-05 11:08:01 +0800

    fix misc

--- kubejs/server_scripts/src/recipes_types/minecraft/smelting.js
+++ kubejs/server_scripts/src/recipes_types/minecraft/smelting.js
@@ -3,8 +3,6 @@ ServerEvents.recipes((event) => {
 
     let recipes = [
         {output: "minecraft:terracotta", input: "minecraft:clay"},
-        {output: 'minecraft:black_dye', input: '#kubejs:coal_dust'},
-        {output: 'minecraft:black_dye', input: 'kubejs:charcoal_dust'},
         // Note: Dirt needs special biomass processing, otherwise becomes coarse_dirt
         {output: 'minecraft:coarse_dirt', input: 'frostedheart:dirt_permafrost'},
         {output: 'minecraft:coarse_dirt', input: 'frostedheart:rooted_dirt_permafrost'},
@@ -16,8 +14,7 @@ ServerEvents.recipes((event) => {
         {output: 'minecraft:red_sand', input: 'frostedheart:red_sand_permafrost'},
         {output: 'frostedheart:peat_block', input: 'frostedheart:peat_permafrost'},
         {output: 'frostedheart:bauxite_block', input: 'frostedheart:bauxite_permafrost'},
-        {output: 'frostedheart:kaolin_block', input: 'frostedheart:kaolin_permafrost'},
-        {output: 'minecraft:paper', input: 'frostedheart:pulp'}
+        {output: 'frostedheart:kaolin_block', input: 'frostedheart:kaolin_permafrost'}
     ]
 	minecraft.smelting('4x minecraft:red_dye', 'frostedheart:lead_oxide_dust')
     minecraft.smelting("frostedheart:ceramic_bucket", "kubejs:clay_bucket")
