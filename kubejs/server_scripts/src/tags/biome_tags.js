6c2afa9480833c79b43a34f5f0509279df1d3a18
yuesha-yc <kevinwangyichen@gmail.com>
2025-05-22 21:57:45 -0700

    TODO

--- kubejs/server_scripts/src/tags/biome_tags.js
+++ kubejs/server_scripts/src/tags/biome_tags.js
@@ -31,6 +31,46 @@ ServerEvents.tags("worldgen/biome", (event) => {
         "minecraft:desert",
     ])
 
+    // TODO: make certain biomes only partially covered with snow
+    event.add("frostedheart:transform_winter_features", [
+        "minecraft:frozen_ocean",
+        "minecraft:deep_frozen_ocean",
+        "minecraft:jagged_peaks",
+        "minecraft:frozen_peaks",
+        "minecraft:grove",
+        "minecraft:snowy_slopes",
+        "minecraft:frozen_river",
+        "minecraft:snowy_beach",
+        // "minecraft:snowy_taiga",
+        // "minecraft:snowy_plains",
+        "minecraft:ice_spikes",
+        "minecraft:badlands",
+        "minecraft:eroded_badlands",
+        "minecraft:desert",
+
+        "the_winter_rescue:ice_caves",
+        "the_winter_rescue:mycelium_caves",
+        "the_winter_rescue:nature/destroyed_marsh",
+        "the_winter_rescue:nature/glacier",
+        "the_winter_rescue:nature/slabback_massif",
+        "the_winter_rescue:nature/frostbough_forest",
+        "the_winter_rescue:nature/ironwinter_hollow",
+        // "the_winter_rescue:nature/snowy_shrubland",
+        "the_winter_rescue:nature/frostpine_grove",
+        "the_winter_rescue:nature/scree_pine_grove",
+        "the_winter_rescue:active_volcano",
+        "the_winter_rescue:alpine_glacier",
+        // "the_winter_rescue:glacial_ices",
+        // "the_winter_rescue:glacial_lakes",
+        "the_winter_rescue:ice_cap",
+        "the_winter_rescue:tidewater_glacier",
+        // "the_winter_rescue:tundra",
+        // "the_winter_rescue:underwater_volcano",
+        "the_winter_rescue:valley_glacier",
+        // "the_winter_rescue:volcanic_hills"
+
+    ])
+
     event.add("frostedheart:water_do_not_freeze", [
         "the_winter_rescue:hydrothermal_deposits",
         "the_winter_rescue:magmatic_deposits",
